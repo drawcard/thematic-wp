d45c319b76efcdddcb7004269e766d73a0280d05
Drawcard <git@drawcard.com.au>
2018-02-19 23:19:52 +1030

    Uikit navbar area

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,10 +1,20 @@
 <header class="banner">
-  <div class="container">
-    <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
-    <nav class="nav-primary">
-      @if (has_nav_menu('primary_navigation'))
-        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
-      @endif
-    </nav>
-  </div>
+  <nav id="navbar-top" class="uk-navbar-container uk-padding-small" uk-navbar>
+      <div class="uk-navbar-left">
+        <a class="brand" href="{{ home_url('/') }}">
+          <img src="@asset('images/logo.svg')">
+        </a>
+      </div>
+      <div class="uk-navbar-center">
+        ...
+         <nav class="nav-primary">
+          @if (has_nav_menu('primary_navigation'))
+            {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
+          @endif
+         </nav>
+      </div>
+      <div class="uk-navbar-right">
+        ...
+    </div>
+  </nav>
 </header>
